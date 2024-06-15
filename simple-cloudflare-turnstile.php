4f64dcfe973788fecf4b3945b4b92694bb016bb4
nisbet-hubbard <87453615+nisbet-hubbard@users.noreply.github.com>
2024-06-15 18:11:37 +0800

    Update simple-cloudflare-turnstile.php

--- simple-cloudflare-turnstile.php
+++ simple-cloudflare-turnstile.php
@@ -196,12 +196,7 @@ if (!empty(get_option('cfturnstile_key')) && !empty(get_option('cfturnstile_secr
 		}
 
 		// Include Elementor Forms
-		if ( cft_is_plugin_active('elementor-pro/elementor-pro.php') ) {
-			include(plugin_dir_path(__FILE__) . 'inc/integrations/other/elementor.php');
-		}
-
-		// Include PRO Elements Forms
-		if ( cft_is_plugin_active('pro-elements/pro-elements.php') ) {
+		if ( cft_is_plugin_active('elementor-pro/elementor-pro.php') || cft_is_plugin_active('pro-elements/pro-elements.php') ) {
 			include(plugin_dir_path(__FILE__) . 'inc/integrations/other/elementor.php');
 		}
 				
